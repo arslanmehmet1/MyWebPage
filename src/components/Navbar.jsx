2d19c83b65b97ce0d407f563b73e0e88077a9411
Mehmet Arslan <marslan.developer@gmail.com>
2023-03-21 18:16:48 +0300

    navbar almost done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 // import { NavLink } from "react-router-dom";
 import "../css/navbar.css";
 
 const Navbar = () => {
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
   return (
     <>
       {/* Demo header*/}
-      <section className="py-5 header text-center topimg">
+      <section className="py-5 text-center topimg">
         <div className="container py-5 text-white">
           <header className="py-5">
             <h1 className="display-2">Hello</h1>
@@ -18,7 +28,19 @@ const Navbar = () => {
         </div>
       </section>
       {/* Sticky navbar*/}
-      <div className="sticky-top"></div>
+      <div className="sticky-top">
+        <div className="topnav" id="myTopnav">
+          <a href="#home" className="active">
+            Home
+          </a>
+          <a href="#news">News</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+          <a href="javascript:void(0);" className="icon" onClick={myFunction}>
+            <i className="fa fa-bars" />
+          </a>
+        </div>
+      </div>
     </>
   );
 };
