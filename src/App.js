943db1a00d5ce7917926ffc32d1c37228d226eee
Mehmet Arslan <marslan.developer@gmail.com>
2023-03-28 13:56:51 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Route path="/blogs/" element={<Blogs />} />
         <Route path="/projects/" element={<Projects />} />
         <Route path="/contact/" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
   );
