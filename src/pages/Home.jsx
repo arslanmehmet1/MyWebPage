18d76e339d0aa00742de9f843b4b0420c3d7d143
Mehmet Arslan <marslan.developer@gmail.com>
2023-04-03 09:26:22 +0300

    resumee

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import "../css/home.css";
 import myimg from "../img/myimg.jpg";
 import { AiOutlineDownload } from "react-icons/ai";
 import world from "../assets/about.gif";
-import dosya from "../assets/Resume.pdf";
+import dosya from "../assets/Resumee";
 
 const Home = () => {
   const navigate = useNavigate();
